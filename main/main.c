30d2c9d2c85632fca67204833b068bc1661aeb65
Ivan Vazquez <foozlevazquez@gmail.com>
2026-03-14 09:58:05 -0700

    Tapping working with new buttons.

--- main/main.c
+++ main/main.c
@@ -41,6 +41,16 @@ static void lv_tick_task(void *arg);
 static void guiTask(void *pvParameter);
 static void create_helloworld_application(void);
 
+
+/****** MY DEFS *****/
+typedef struct {
+    lv_obj_t *btn;
+    char *name;
+    int count;
+} mybutton_t;
+
+static mybutton_t mybuttons[4];
+
 /**********************
  *   APPLICATION MAIN
  **********************/
@@ -154,32 +164,70 @@ static void guiTask(void *pvParameter) {
     vTaskDelete(NULL);
 }
 
-static lv_obj_t *corner_btns[4];
 static const char *corner_names[4] = {
     "top-left", "top-right", "bottom-left", "bottom-right"
 };
 
-static void corner_btn_cb(lv_obj_t *btn, lv_event_t event)
+/* static void corner_btn_cb(lv_obj_t *btn, lv_event_t event) */
+/* { */
+/*     if (event != LV_EVENT_PRESSED) return; */
+/*     for (int i = 0; i < 4; i++) { */
+/*         lv_indev_t *indev = lv_indev_get_act(); */
+/*         lv_point_t pt; */
+/*         lv_indev_get_point(indev, &pt); */
+
+/*         printf("HIT \"%s\"  raw(%d,%d)  mapped(%d,%d)  hitbox x=%d y=%d w=%d h=%d\n", */
+/*             corner_names[i], */
+/*             ft6x36_last_raw_x, ft6x36_last_raw_y, */
+/*             pt.x, pt.y, */
+/*             lv_obj_get_x(btn), lv_obj_get_y(btn), */
+/*             lv_obj_get_width(btn), lv_obj_get_height(btn)); */
+/*        break; */
+/*     } */
+/* } */
+
+static void btn_event_cb(lv_obj_t * btn, lv_event_t event)
 {
-    if (event != LV_EVENT_PRESSED) return;
-    for (int i = 0; i < 4; i++) {
-        if (btn != corner_btns[i]) continue;
+    if(event == LV_EVENT_CLICKED) {
+        mybutton_t *btn_data = (mybutton_t *) lv_obj_get_user_data(btn);
+        btn_data->count++;
+
         lv_indev_t *indev = lv_indev_get_act();
         lv_point_t pt;
         lv_indev_get_point(indev, &pt);
-        printf("HIT \"%s\"  raw(%d,%d)  mapped(%d,%d)  hitbox x=%d y=%d w=%d h=%d\n",
-            corner_names[i],
-            ft6x36_last_raw_x, ft6x36_last_raw_y,
-            pt.x, pt.y,
-            lv_obj_get_x(btn), lv_obj_get_y(btn),
-            lv_obj_get_width(btn), lv_obj_get_height(btn));
-        break;
+
+        printf("HIT \"%s\"  raw(%d,%d)  mapped(%d,%d)",
+               btn_data->name, ft6x36_last_raw_x, ft6x36_last_raw_y, pt.x, pt.y);
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, NULL);
+        lv_label_set_text_fmt(label, "Button: %d", btn_data->count);
+    }
+}
+
+
+static void screen_event_cb(lv_obj_t *scr, lv_event_t event) {
+    if (event==LV_EVENT_CLICKED) {
+
+        lv_indev_t *indev = lv_indev_get_act();
+        lv_point_t pt;
+        lv_indev_get_point(indev, &pt);
+
+        lv_obj_t *btn = lv_btn_create(scr, NULL);
+        lv_obj_set_size(btn, 10, 10);
+
+        lv_obj_set_x(btn, pt.x);
+        lv_obj_set_y(btn, pt.y);
     }
 }
 
 static void create_helloworld_application(void)
 {
     lv_obj_t *scr = lv_scr_act();
+
+    lv_obj_set_event_cb(scr, screen_event_cb);
+
+
 
     static const lv_align_t aligns[4] = {
         LV_ALIGN_IN_TOP_LEFT,
@@ -193,19 +241,24 @@ static void create_helloworld_application(void)
 
     for (int i = 0; i < 4; i++) {
         lv_obj_t *btn = lv_btn_create(scr, NULL);
-        lv_obj_set_size(btn, 90, 50);
+        lv_obj_set_size(btn, 20, 20);
         lv_obj_align(btn, NULL, aligns[i], offsets[i].x, offsets[i].y);
-        lv_obj_set_event_cb(btn, corner_btn_cb);
-        corner_btns[i] = btn;
+        lv_obj_set_event_cb(btn, btn_event_cb);
+        lv_obj_set_user_data(btn, &mybuttons[i]);
+        mybuttons[i].name = corner_names[i];
+        mybuttons[i].count = 0;
+        mybuttons[i].btn = btn;
 
         lv_obj_t *lbl = lv_label_create(btn, NULL);
         lv_label_set_text(lbl, corner_names[i]);
-
+        lv_obj_set_event_cb(btn, btn_event_cb);
         printf("btn \"%s\" hitbox: x=%d y=%d w=%d h=%d\n",
             corner_names[i],
             lv_obj_get_x(btn), lv_obj_get_y(btn),
             lv_obj_get_width(btn), lv_obj_get_height(btn));
     }
+
+
 }
 
 static void lv_tick_task(void *arg) {
